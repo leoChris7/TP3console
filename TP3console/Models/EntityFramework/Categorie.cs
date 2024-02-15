fe9f274e16f568302a7ef263307a47799e6c02dd
Léo Christophe <Leo.Christophe@etu.univ-savoie.fr>
2024-02-15 09:13:20 +0100

    Ajoutez des fichiers projet.

---
+++ TP3console/Models/EntityFramework/Categorie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace TP3console.Models.EntityFramework
+{
+    [Table("categorie")]
+    public partial class Categorie
+    {
+        public Categorie()
+        {
+            Films = new HashSet<Film>();
+        }
+
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("nom")]
+        [StringLength(50)]
+        public string Nom { get; set; } = null!;
+        [Column("description")]
+        public string? Description { get; set; }
+
+        [InverseProperty("CategorieNavigation")]
+        public virtual ICollection<Film> Films { get; set; }
+    }
+}
